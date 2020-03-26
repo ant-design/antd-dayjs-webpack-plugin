d5a935af3e8b721bb809d3e02f5d3b2f9e05b500
iamkun <kunhello@outlook.com>
2020-03-26 17:53:39 +0800

    update antd v4

--- src/index.js
+++ src/index.js
@@ -14,6 +14,21 @@ const presets = {
       'isMoment',
       'localeData',
       'localizedFormat',
+    ],
+    replaceMoment: true
+  },
+  antdv3: {
+    plugins: [
+      'isSameOrBefore',
+      'isSameOrAfter',
+      'advancedFormat',
+      'customParseFormat',
+      'weekday',
+      'weekYear',
+      'weekOfYear',
+      'isMoment',
+      'localeData',
+      'localizedFormat',
       'badMutable'
     ],
     replaceMoment: true
