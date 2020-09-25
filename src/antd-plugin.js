ae90784e363412459dc6c4b4ab44393fa695edc7
iamkun <kunhello@outlook.com>
2020-09-25 14:58:57 +0800

    chore: update antd-plugin.js to use es5 syntax
    fix #39

--- src/antd-plugin.js
+++ src/antd-plugin.js
@@ -1,17 +1,17 @@
-const localeMap = {
+var localeMap = {
   en_GB: 'en-gb',
   en_US: 'en',
   zh_CN: 'zh-cn',
   zh_TW: 'zh-tw'
 };
 
-const parseLocale = function parseLocale(locale) {
+var parseLocale = function parseLocale(locale) {
   var mapLocale = localeMap[locale];
   return mapLocale || locale.split('_')[0];
 };
 
 module.exports = function (option, dayjsClass, dayjsFactory) {
-  const oldLocale = dayjsClass.prototype.locale
+  var oldLocale = dayjsClass.prototype.locale
   dayjsClass.prototype.locale = function(arg) {
     if (typeof arg === 'string') {
       arg = parseLocale(arg)
